80b450139c37c71fab0cc2ad7a1cab746f207128
Nicholson Galela <nicholsong@saica.co.za>
2024-06-27 14:34:38 +0200

    remove commented out code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { GithubRepositoryListComponent } from './views/github-repository-list/github-repository-list.component';
-// export {
-//   MatCardAppearance,
-//   MatCardConfig,
-//   MAT_CARD_CONFIG,
-//   MatCard,
-//   MatCardTitle,
-//   MatCardTitleGroup,
-//   MatCardContent,
-//   MatCardSubtitle,
-//   MatCardActions,
-//   MatCardHeader,
-//   MatCardFooter,
-//   MatCardImage,
-//   MatCardSmImage,
-//   MatCardMdImage,
-//   MatCardLgImage,
-//   MatCardXlImage,
-//   MatCardAvatar
-// }
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
