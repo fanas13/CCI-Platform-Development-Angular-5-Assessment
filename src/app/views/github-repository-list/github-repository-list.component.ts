26307fdfa1a4dd5df5e81807d84c3831d6594e22
Nicholson Galela <nicholsong@saica.co.za>
2024-06-27 12:00:06 +0200

    update the routes.ts to app-routing.module.ts, change moduleResolution to node, and also create new components for listing

---
+++ src/app/views/github-repository-list/github-repository-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-repository-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './github-repository-list.component.html',
+  styleUrl: './github-repository-list.component.css'
+})
+export class GithubRepositoryListComponent {
+
+}
