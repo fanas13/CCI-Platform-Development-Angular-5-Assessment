d67d8bca3cddd4000a146c658276fb416ae26dfd
Nicholson Galela <nicholsong@saica.co.za>
2024-06-27 15:07:36 +0200

    created new components for issues list

---
+++ src/app/views/github-issue-list/github-issue-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-issue-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './github-issue-list.component.html',
+  styleUrl: './github-issue-list.component.css'
+})
+export class GithubIssueListComponent {
+
+}
