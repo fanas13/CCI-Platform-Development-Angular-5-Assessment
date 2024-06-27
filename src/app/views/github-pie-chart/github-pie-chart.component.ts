97bba6be7ae7e16f83c5ba21d346e49976f98f2e
Nicholson Galela <nicholsong@saica.co.za>
2024-06-27 15:28:38 +0200

    created new components for pie chart

---
+++ src/app/views/github-pie-chart/github-pie-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-pie-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './github-pie-chart.component.html',
+  styleUrl: './github-pie-chart.component.css'
+})
+export class GithubPieChartComponent {
+
+}
