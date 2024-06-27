70a600fe6bdc3fecb57b76a9233ad6a2937e344b
Nicholson Galela <nicholsong@saica.co.za>
2024-06-27 13:21:03 +0200

    created an endpoint method call to search for repositories

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  private apiUrl = 'https://api.github.com';
+
+  constructor(private http: HttpClient) { }
+
+  searchRepositories(query: string): Observable<any> {
+    const url = `${this.apiUrl}/search/repositories?q=${query}`;
+    return this.http.get(url);
+  }
+
 }
